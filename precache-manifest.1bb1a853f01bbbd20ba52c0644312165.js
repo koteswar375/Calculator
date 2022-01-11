fea499b62414aca88c01775c423e3ee062b8ae66
koti meesala <kotislr75@gmail.com>
2022-01-10 19:32:06 -0600

    Updates

---
+++ precache-manifest.1bb1a853f01bbbd20ba52c0644312165.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "297df451890ca2b0023a505dc82eec81",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "fc512a5d968d8ce632bd",
+    "url": "/Calculator/static/css/main.3b7fe433.chunk.css"
+  },
+  {
+    "revision": "329ddf826f9d4557fe7b",
+    "url": "/Calculator/static/js/2.c0b11794.chunk.js"
+  },
+  {
+    "revision": "fc512a5d968d8ce632bd",
+    "url": "/Calculator/static/js/main.a01d99d8.chunk.js"
+  },
+  {
+    "revision": "cd2f8dea5990ba4ebde0",
+    "url": "/Calculator/static/js/runtime-main.0580d45d.js"
+  }
+]);
